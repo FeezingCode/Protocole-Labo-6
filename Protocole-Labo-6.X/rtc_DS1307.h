8bde5aa16279c840ed5cc6795ed5bdd5fe907581
Asgard20032 <pogo20032@hotmail.com>
2014-04-08 10:52:15 -0400

    Created Project

---
+++ Protocole-Labo-6.X/rtc_DS1307.h
@@ -0,0 +1,25 @@
+/* 
+ * File:   rtc_DS1307.h
+ * Author: a.linteau
+ *
+ * Created on 8 avril 2014, 09:54
+ */
+
+#ifndef RTC_DS1307_H
+#define	RTC_DS1307_H
+
+#include <plib/i2c.h>
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+    
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* RTC_DS1307_H */
+
---
+++ Protocole-Labo-6.X/sensor_distance_SRF02.h
@@ -0,0 +1,25 @@
+/* 
+ * File:   sensor_distance_SRF02.h
+ * Author: a.linteau
+ *
+ * Created on 8 avril 2014, 09:55
+ */
+
+#ifndef SENSOR_DISTANCE_SRF02_H
+#define	SENSOR_DISTANCE_SRF02_H
+
+#include <plib/i2c.h>
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* SENSOR_DISTANCE_SRF02_H */
+
