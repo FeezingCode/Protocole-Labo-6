c73d0fca9729b27fd5d63d1a3a44b6f231d7ade3
Barn18 <gui.lagrange18@hotmail.com>
2014-04-08 10:55:37 -0400

    New main.c

---
+++ Protocole-Labo-6.X/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: g.lagrange
+ *
+ * Created on 8 avril 2014, 10:55
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
